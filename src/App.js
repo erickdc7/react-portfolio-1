43f64e6744eec4ba76e6478dbd51cc6b639e3160
ErickDC <erickdiazc717@gmail.com>
2023-01-31 13:46:04 -0500

    qualification section added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import About from './components/about/About'
 import Header from './components/header/Header'
 import Home from './components/home/Home'
+import Qualification from './components/qualification/Qualification'
 import Services from './components/services/Services'
 import Skills from './components/skills/Skills'
 
@@ -16,6 +17,7 @@ const App = () => {
         <About />
         <Skills />
         <Services />
+        <Qualification />
       </main>
     </div>
   )
