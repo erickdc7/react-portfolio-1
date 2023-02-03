1b18bdfc0799a8bc2af16ab1647389e30c40797d
ErickDC <erickdiazc717@gmail.com>
2023-02-03 13:31:01 -0500

    scroll-to-top component added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/footer/Footer'
 import Header from './components/header/Header'
 import Home from './components/home/Home'
 import Qualification from './components/qualification/Qualification'
+import ScrollUp from './components/scrollup/ScrollUp'
 import Services from './components/services/Services'
 import Skills from './components/skills/Skills'
 import Testimonials from './components/testimonials/Testimonials'
@@ -23,9 +24,10 @@ const App = () => {
         <Qualification />
         <Testimonials />
         <Contact />
-        <Footer />
       </main>
 
+      <Footer />
+      <ScrollUp />
     </div>
   )
 }
---
+++ src/components/scrollup/ScrollUp.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './scrollup.css'
+
+const ScrollUp = () => {
+    window.addEventListener("scroll", function () {
+        const scrollUp = document.querySelector(".scrollup");
+
+        // when the scroll is higher than 560 viewport height, add the show-scroll class to a tag with the scroll-top class
+        if (this.scrollY >= 560) {
+            scrollUp.classList.add("show-scroll")
+        } else {
+            scrollUp.classList.remove("show-scroll")
+        }
+    })
+
+    return (
+        <a href="#home" className="scrollup">
+            <i className="uil uil-arrow-up scrollup__icon"></i>
+        </a>
+    )
+}
+
+export default ScrollUp
