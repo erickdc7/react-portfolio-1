476bf9e10aeb01e0aaef32967eb4ad64b7a259ca
ErickDC <erickdiazc717@gmail.com>
2023-04-09 17:44:39 -0500

    work component updated

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './work.css'
+import Works from './Works'
 
 const Work = () => {
     return (
-        <div>Work</div>
+        <section className="work section" id='portfolio'>
+            <h2 className="section__title">Portfolio</h2>
+            <span className="section__subtitle">Most recent works</span>
+            <Works />
+        </section>
     )
 }
 
