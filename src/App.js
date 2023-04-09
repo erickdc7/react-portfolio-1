e83712577204bbf4b78f6d20121c2747de8afd04
ErickDC <erickdiazc717@gmail.com>
2023-04-09 14:05:33 -0500

    work component created

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import ScrollUp from './components/scrollup/ScrollUp'
 import Services from './components/services/Services'
 import Skills from './components/skills/Skills'
 import Testimonials from './components/testimonials/Testimonials'
+import Work from './components/work/Work'
 
 const App = () => {
   return (
@@ -22,6 +23,7 @@ const App = () => {
         <Skills />
         <Services />
         <Qualification />
+        <Work />
         <Testimonials />
         <Contact />
       </main>
---
+++ src/components/work/Work.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './work.css'
+
+const Work = () => {
+    return (
+        <div>Work</div>
+    )
+}
+
+export default Work
